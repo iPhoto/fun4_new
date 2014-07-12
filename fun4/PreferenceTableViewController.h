526249968cbedb1e6b558928881ddd0058642bf7
bluebeans <niyan@Nis-MacBook-Air.local>
2014-07-12 16:52:45 -0400

    preference table view

---
+++ fun4/PreferenceTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PreferenceTableViewController.h
+//  fun4
+//
+//  Created by Ni Yan on 7/12/14.
+//  Copyright (c) 2014 Ni Yan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PreferenceTableViewController : UITableViewController
+
+@end
